18120de29038d80088f9db732bc96125b5c6298f
marie-laurence <charlotte.marielaurence@neuf.fr>
2022-03-30 15:33:12 +0200

    ajouter selenium

---
+++ src/test/java/SeleniumTests.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTests {
+
+  @Test
+public void seleniumTEST () {
+    WebDriver driver = new ChromeDriver();
+      driver.get("https://www.backmarket.fr");
+
+
+
+
+    driver.quit();
+
+
+
+  }
+
+
+}
